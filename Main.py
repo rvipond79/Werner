b3871a4d4cd7c85399a080a24afd91ab7836f1e4
rvipond79 <rvipond79@gmail.com>
2023-02-27 17:16:02 -0500

    newest changes

--- Main.py
+++ Main.py
@@ -11,15 +11,20 @@ driver.find_element(By.ID, "twotabsearchtextbox").send_keys("socks")
 driver.find_element(By.ID, "nav-search-submit-button").click()
 #scroll down to the bottom of the page
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#sleep for 2 seconds
+time.sleep(2)
+#scroll up to the top of the page
+driver.execute_script("window.scrollTo(0, 0);")
+#sleep for 2 seconds
+time.sleep(2)
+#click Sort by dropdown and select "Price: High to Low"
+driver.find_element(By.ID, "a-autoid-0-announce").click()
+driver.find_element(By.ID, "s-result-sort-select_2").click()
+time.sleep(2)
+#click Eligible for Free Shipping checkbox
+driver.find_element(By.ID, "p_76/2661625011").click()
+#select an item and add it to the cart
+driver.find_element(By.ID, "add-to-cart-button").click()
+#click on the cart icon
+driver.find_element(By.ID, "nav-cart").click()
 
-
-
-
-
-
-
-
-
-
-#sleep for 5 seconds
-time.sleep(10)
